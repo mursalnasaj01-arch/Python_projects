d3efbd7e0394418d9da617fe5c36b3a37716ef63
mursalnasaj01-arch <mursalnasaj01@gmail.com>
2025-12-11 09:02:06 -0500

    complex number creating

---
+++ project08.py
@@ -0,0 +1,7 @@
+# Complex number creating
+
+real=float(input("enter the real part: "))
+imag=float(input("enter the imaginary part: "))
+
+complex_number=complex(real,imag)
+print(f"the complex number is:{complex_number}")
