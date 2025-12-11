e6fd68fbb88246e99b7a3234d5dba04247b42057
mursalnasaj01-arch <mursalnasaj01@gmail.com>
2025-12-10 20:11:55 -0500

    Area of a Parallelogram

---
+++ project03.py
@@ -0,0 +1,5 @@
+#Parallelogram area
+base=float(input("Enter the base of parallelogram: "))
+height=float(input("Enter the height of parallelogram: "))
+area=base*height
+print(f"The area of Parallelogram is equal to: {area}")
