12be1e5d420b2efaf215967541b24edd0d6c9d02
mursalnasaj01-arch <mursalnasaj01@gmail.com>
2025-12-11 15:55:01 -0500

    Calculate Factorials Recursively

---
+++ project13.py
@@ -0,0 +1,8 @@
+#Calculate Factorials Recursively
+
+def factorial(n):
+    if n==0 or n==1: return 1
+    return n*factorial(n-1)
+
+number = int(input("enter a number: "))
+print(f"the factorial of {number} is {factorial(number)}")
