ff22e3ce252493f3dbb552c4254e4ce934470df6
mursalnasaj01-arch <mursalnasaj01@gmail.com>
2025-12-15 07:06:40 -0500

    Convert Decimal to Binary, Octal and Hexadecimal

---
+++ project16.py
@@ -0,0 +1,11 @@
+# Convert Decimal to Binary, Octal and hexadecimal
+
+num=int(input("enter a number: "))
+binary=bin(num)
+octal=oct(num)
+hexadecimal=hex(num)
+
+print(f"Binary:{binary}")
+print(f"Octal:{octal}")
+print(f"hexadecimal: {hexadecimal}")
+
