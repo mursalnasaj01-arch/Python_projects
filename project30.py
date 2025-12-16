cae37208dd0c9c87ccac119ef2e90b210d6f0bda
mursalnasaj01-arch <mursalnasaj01@gmail.com>
2025-12-16 11:22:39 -0500

    Check if a string is alphanumeric

---
+++ project30.py
@@ -0,0 +1,7 @@
+# Check if a string is alphanumeric
+
+text=input("Enter a string: ")
+if text.isalnum():
+    print(f"{text} is alphanumeric.")
+else:
+    print(f"{text} is not alphanumeric.")    
