fac08544d193e94658fdaad56c3d82d0a405596c
mursalnasaj01-arch <mursalnasaj01@gmail.com>
2025-12-16 08:42:47 -0500

    Find the largest Number among a list

---
+++ project21.py
@@ -0,0 +1,6 @@
+#Find the largest Number among a list
+
+Numbers=list(map(int,input("Enter numbers separated by spaces: ").split()))
+
+largest=max(Numbers)
+print(f"the largest number is: {largest}") 
